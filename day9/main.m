a7ca0b0cb405ddc35d23bdf789a82e713d1cc00b
user <iOS-School-1@iMac-user-4.local>
2017-04-15 12:22:19 +0300

    Initial Commit

---
+++ day9/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  day9
+//
+//  Created by iOS-School-1 on 15.04.17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
