d77579f7bddaa9bee375595cf47195f3974c63c9
user <iOS-School-1@iMac-user-4.local>
2017-04-18 18:25:38 +0300

    initial commit

--- day9/main.m
+++ day9/main.m
@@ -8,10 +8,69 @@
 
 #import <Foundation/Foundation.h>
 
+@interface BlockTestClass : NSObject
+
+- (void)testMemory;
+
+@end
+
+@interface BlockTestClass(){
+    NSString *_str2;
+}
+
+@property(nonatomic, copy) NSString *str;
+@property(nonatomic, copy) void (^test)(void);
+@end
+
+@implementation BlockTestClass
+
+-(instancetype)init{
+    self = [super init];
+    if(self){
+        _str = @"Hello World!!";
+        _str2 = @"Hello World!";
+    }
+    return self;
+    
+}
+
+-(void)testMemory{
+    //__weak typeof(self) weakSelf = self; //to prevent retain cycle
+    __weak typeof(self) weakSelf = self;
+    self.test = ^void(void){
+        //__strong typeof(self) strongSelf = weakSelf; //to keep the reference in case object is destroyed before block ends
+        //NSLog(@"%@", strongSelf.str);
+        __strong typeof(self) strongSelf = weakSelf;
+        NSLog(@"%@", strongSelf -> _str2);
+    };
+    self.test();
+}
+
+@end
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //BlockTestClass *test = [BlockTestClass new];
+        //[test testMemory];
+        NSArray *arr = @[@(1),@(2),@(4),@(3),@(5)];
+        [arr enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if(3 == [obj intValue]){
+                NSLog(@"%@", @(idx));
+                //stop = YES;
+            }
+        }];
+        
+        /*dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                      ^{
+                          NSLog(@"Hello world!");
+                      });
+        __block int a = 0;
+        int (^inc)(void) = ^int(void){
+            return ++a;
+        };
+        //inc(); - stack block (deleted out of scope)
+        //[inc copy] - heap/malloc block (can be accessed from outside)*/
     }
     return 0;
 }
